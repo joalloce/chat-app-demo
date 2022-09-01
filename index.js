5b4e92c0689c1198d9ffcd26903a9de7eebc0325
Jose Lo <joalloce@gmail.com>
2022-09-01 01:22:54 -0600

    sending messages done. basic functionality done.

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("io connected");
+  console.log("socket connected", socket.id);
+  socket.on("disconnect", () => {
+    console.log("socket disconnected", socket.id);
+  });
+  socket.on("chat message", (message) => {
+    io.emit("chat message", message);
+  });
 });
 
 app.use(express.static(path.join(__dirname, "public")));
--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,29 @@
 const socket = io();
-console.log("main.js");
+
+const username = getUsername();
+console.log(username);
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("chat message", (message) => {
+  const item = document.createElement("li");
+  item.textContent = message;
+  messages.appendChild(item);
+});
+
+function getUsername() {
+  const queryString = location.search;
+  const params = new URLSearchParams(queryString);
+  const username = params.get("username");
+  return username;
+}
